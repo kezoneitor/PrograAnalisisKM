9881e459b9da8b24470c0d1dc2a94beb598bd492
Keslerth <keslerth.c@gmail.com>
2017-10-18 11:10:53 -0600

    Cambio de la matriz

--- PrograAnalisisKM/Program.cs
+++ PrograAnalisisKM/Program.cs
@@ -8,26 +8,29 @@ namespace PrograAnalisisKM
 {
     class Program
     {
-        static int[,,] matriz;
+        static int[][][] matriz;
+
         static void crearMatrizdeJuego(int n)
         {
-            matriz = new int[n, n, 4];
+            matriz = new int[n][][];
             Random rand = new Random();
             for (int i = 0; i < n; i++)
             {
-                for (int j; j < n; j++)
+                matriz[i] = new int[n][];
+                for (int j = 0; j < n; j++)
                 {
-                    matriz[i, j, 0] = rand.Next(1, 23);
-                    matriz[i, j, 1] = rand.Next(1, 23);
-                    matriz[i, j, 2] = rand.Next(1, 23);
-                    matriz[i, j, 3] = rand.Next(1, 23);
+                    matriz[i][j] = new int[4];
+                    matriz[i][j][0] = rand.Next(1, 23);
+                    matriz[i][j][1] = rand.Next(1, 23);
+                    matriz[i][j][2] = rand.Next(1, 23);
+                    matriz[i][j][3] = rand.Next(1, 23);
                     if (i - 1 >= 0)
                     {
-                        matriz[i, j, 0] = matriz[i - 1, j, 2];
+                        matriz[i][j][0] = matriz[i - 1][j][2];
                     }
                     if (j - 1 >= 0)
                     {
-                        matriz[i, j, 3] = matriz[i, j - 1, 1];
+                        matriz[i][j][3] = matriz[i][j - 1][1];
                     }
                 }
             }
@@ -38,16 +41,23 @@ namespace PrograAnalisisKM
                 while (j < n)
                 {
                     if (i == 0)
-                        matriz[i, j, 0] = 0;
+                        matriz[i][j][0] = 0;
                     if (j == 0)
-                        matriz[i, j, 3] = 0;
+                        matriz[i][j][3] = 0;
                     if (i == n - 1)
-                        matriz[i, j, 2] = 0;
+                        matriz[i][j][2] = 0;
                     if (j == n - 1)
-                        matriz[i, j, 1] = 0;
+                        matriz[i][j][1] = 0;
                     j++;
                 }
             }
+        }
+
+        static void RevolverMatriz(int n)
+        {
+
+            Random rand = new Random();
+
         }
 
         static void imprimirMatriz(int n)
@@ -60,7 +70,7 @@ namespace PrograAnalisisKM
                     m += "<";
                     for (int k = 0; k < 4; k++)
                     {
-                        m += matriz[i, j, k] + ",";
+                        m += matriz[i][j][k] + ",";
                     }
                     m += ">\t";
                     if (j == n - 1)
